73eb0bdf1b65bcad3282e472e43f4171c0eda86c
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-05-28 10:21:19 +0100

    Adding a new helper : is_multi_array

--- helpers.php
+++ helpers.php
@@ -15,3 +15,16 @@ if ( ! function_exists('hash_id')) {
         return md5($id . serialize($options));
     }
 }
+
+if ( ! function_exists('is_multi_array')) {
+    /**
+     * Check if the array is a multidimensional array
+     *
+     * @param  array   $array
+     *
+     * @return boolean
+     */
+    function is_multi_array(array $array) {
+        return is_array(head($array));
+    }
+}
